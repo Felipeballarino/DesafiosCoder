a1da21cc6b5efec31537d38cbce2e94e5ae26d0e
Felipeballarino <Felipeballarino97@gmail.com>
2022-04-13 11:44:52 -0300

    13/04

--- Desafio3/index.js
+++ Desafio3/index.js
@@ -3,28 +3,28 @@ const fs = require('fs')
 const app = express();
 const PORT = 8080
 
+const Contenedor = require("./desafio_2.js")
+const file = new Contenedor("productos.txt")
 
-app.get('/productos', (req, res) => {
-    fs.readFile("./productos.txt", "utf-8", (error, contenido) => {
-        const data = JSON.parse(contenido)
-        res.send({
-            menssage: "Array con todos los productos disponibles",
-            data: data
-        })
+
+app.get('/productos', async (req, res) => {
+    const product = file.getAll()
+    // console.log(producto)
+    res.send({
+        menssage: "Array con todos los productos disponibles",
+        data: JSON.parse(product)
     })
 })
 
 app.get('/productoRandom', (req, res) => {
-    fs.readFile("./productos.txt", "utf-8", (error, contenido) => {
-        const data = JSON.parse(contenido)
-        let indice = Math.floor(Math.random()* data.length)
-        let newPrdouct = data[indice]
-        res.send({
-            menssage: "Array con un producto al azaar",
-            data: newPrdouct
-        })
+    const procucto = file.getAll()
+    const data = JSON.parse(procucto)
+    let indice = Math.floor(Math.random() * data.length)
+    let newPrdouct = data[indice]
+    res.send({
+        menssage: "Array con un producto al azaar",
+        data: newPrdouct
     })
-
 })
 
 
